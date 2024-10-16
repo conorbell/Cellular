858f31c150010c64db8cea4d6f3a1bd58a459fa5
conorbell <conorbell27@gmail.com>
2024-10-16 13:03:52 -0400

    hiding screenings on mobile

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,12 @@
 export default function Modal({ movie, context, festival }) {
   return (
-    <section className="fixed  inset-0 flex justify-center items-center bg-black bg-opacity-50">
-      <div className="relative w-[90%] max-w-[640px] bg-white rounded-lg shadow-lg">
+    <section className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
+      <div className="relative w-full max-w-[640px] bg-white shadow-lg md:rounded-lg">
         {/* Video Section */}
         <div className="w-full">
           <iframe
             title="movie"
-            className="w-full h-[56.25vw] max-h-[360px] rounded-t-lg"
+            className="w-full h-[56.25vw] md:rounded-t-lg max-h-[360px]"
             src={movie}
             loading="lazy"
             allow="autoplay; fullscreen; picture-in-picture"
@@ -19,8 +19,9 @@ export default function Modal({ movie, context, festival }) {
           <p className="break-words">{context}</p>
         </div>
 
+        {/* Screenings Section - Hidden on Mobile */}
         {festival && (
-          <div className="p-4 text-sm bg-gray-800  text-white">
+          <div className="p-4 text-sm bg-gray-800 text-white sm:hidden">
             <h2 className="font-bold mb-2">Screenings:</h2>
             {Array.isArray(festival) ? (
               <ul className="list-disc list-inside">
